fdf0c4e557caaf1a77c8e89608763ed840157a3b
zhill24237 <126599558+zhill24237@users.noreply.github.com>
2023-05-03 22:44:42 -0400

    Upload Article images semi-update
    Added the ability to upload images, cannot see them on page yet, also using try catch to work with errors

--- CMS/admin/edit-article-image.php
+++ CMS/admin/edit-article-image.php
@@ -29,7 +29,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
         if(empty($_FILES)){
             throw new Exception('Invalid Upload');
         }
-    }
+
         switch($_FILES['file']['error']){
             case UPLOAD_ERR_OK:
                 break;
@@ -39,8 +39,49 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
                 throw new Exception('File is too large (from server settings)');
             default:
                 throw new Exception('An error occurred');
-        } 
+        }
 
+        if($_FILES['file']['size'] > 1000000){
+            throw new Exception('File is too large');
+        }
+
+        $mim_types = ['image/gif', 'image/png', 'image/jpeg'];
+
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mime_type = finfo_file($finfo, $_FILES['file']['tmp_name']);
+
+        if( ! in_array($mime_type, $mim_types)){
+            throw new Exception('Not a supported file type');
+        }
+
+        $pathinfo = pathinfo($_FILES['file']['name']);
+
+        $base = $pathinfo['filename'];
+
+        $base = preg_replace('/[^a-zA-Z0-9_-]/', '_', $base);
+
+        $filename = $base . "." . $pathinfo['extension'];
+
+        $destination = __DIR__ . "/uploads/$filename";
+
+        $i = 1;
+
+        while (file_exists($destination)){
+            $filename = $base . "-$i." . $pathinfo['extension'];
+            $destination = __DIR__ . "/uploads/$filename";
+            $i++;
+        }
+
+        if(move_uploaded_file($_FILES['file']['tmp_name'], $destination)){
+            echo "File uploaded successfully";
+        }else{
+            throw new Exception("File failed to upload");
+        }
+
+    }catch(Exception $e){
+        echo $e->getMessage();
+    } 
+    
 
 }
 ?>
